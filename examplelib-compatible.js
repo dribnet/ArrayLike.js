01a7eddb68e9522790365e4921c93330388ecabe
Tom White <tom@sixdozen.com>
2013-02-17 01:47:41 -0800

    example: a non-conforming library working with a polyfill
    The examplelib in examplelib-compatible has two methods
    which use two different array checking techniques on their
    arguement. Both use JavaScript best practices, but neither
    conforms to the isArray spec, and so as written they will
    only work on native JavaScript array objects.
    
    The isArray-polyfill patches the Object.prototype.toString
    and Array.isArray functions so that they themselves return
    results that conform to the isArray spec.
    
    The polyfill.html has a small snippet to test this funcitonality.
    First calling the examplelib on native JavaScript arrays, and
    then calling them on triplet objects, which conform to the
    isArray spec and advertise their ability to implmenet
    JavaScript array functions.

---
+++ examplelib-compatible.js
@@ -0,0 +1,50 @@
+/**
+ *  Simple example of a library that conforms to isArray array checking
+ */
+
+(function(window, document, undefined) {
+
+var toString          = Object.prototype.toString,
+    examplelib        = window.examplelib || (window.examplelib = {});
+
+// main isArray method
+function isArray(obj) {
+    return (Object.prototype.toString.call(obj) === '[object Array]');
+}
+
+// this is just for testing - usually a library would only have the one above
+function isArraySecondary(obj) {
+    return (Array.isArray(obj));
+}
+
+// given an array, show the result of addition of elements
+function displaySum(arr) {
+  if(!isArray(arr)) {
+    alert("displaySum: Sorry " + arr + " is not an array");
+    return;
+  }
+  var sum = 0;
+  for(var i=0; i<arr.length; i++) {
+    sum += arr[i];
+  }
+  document.body.innerHTML += arr.join(" + ") + " = " + sum + "<p>";
+}
+
+// given an array, show the result of multiplication of elements
+function displayProduct(arr) {
+  if(!isArraySecondary(arr)) {
+    alert("displayProduct: Sorry " + arr + " is not an array");
+    return;
+  }
+  var product = 1;
+  arr.forEach(function (element) {
+    product *= element;
+  });
+  document.body.innerHTML += arr.join(" * ") + " = " + product + "<p>";
+}
+
+// publish external api
+examplelib.displaySum = displaySum;
+examplelib.displayProduct = displayProduct;
+
+})(window, document);
